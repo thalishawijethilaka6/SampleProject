a0582ecd2f695b887c6ec2ddbe0c969006863b7c
thalishawijethilaka6 <thalishawijethilaka6@gmail.com>
2019-05-30 09:12:07 +0530

    Main Upload
    All files may not contain

---
+++ WebApplication2/WebApplication2/Controllers/CustomerController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SampleProject.IRepository;
+
+namespace SampleProject.Controllers
+{
+    [Route("api/Customer")]
+ 
+
+    public class CustomerController : ControllerBase
+    {
+        private ICustomerRepository customerRepository;
+
+        public CustomerController(ICustomerRepository customerRepository)
+        {
+            this.customerRepository = customerRepository;
+
+        }
+        [HttpGet]
+        public string GetName()
+        {
+            string name = customerRepository.GetCustomer();
+            return name;
+        }
+    }
+}
---
+++ WebApplication2/WebApplication2/RepositoryImplement/CustomerRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SampleProject.IRepository;
+
+namespace SampleProject.RepositoryImplement
+{
+    public class CustomerRepository : ICustomerRepository
+    {
+        public string GetCustomer()
+        {
+            return "Thalisha";
+        }
+    }
+}
